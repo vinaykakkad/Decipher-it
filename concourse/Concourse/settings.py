342b67347a3ef0fc657e1584e557bdc8345a82e9
vinay kakkad <imkkad@gmail.com>
2021-02-04 01:23:53 +0530

    removed heroku configurations

--- concourse/Concourse/settings.py
+++ concourse/Concourse/settings.py
@@ -2,7 +2,6 @@ import os
 from decouple import config
 from datetime import datetime
 
-import dj_database_url 
 # import sentry_sdk
 # from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,13 +143,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 #For CKEDITOR
 CKEDITOR_UPLOAD_PATH = "uploads/"
